86411088e6ffea276771e2e884e1f238c758e3a2
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-07-22 10:09:25 +0530

    build

---
+++ build/precache-manifest.213af588ab958f7f23693c4c0aa4eca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542db828cd8e0d6d484dc7320555e1d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fdbe0de5cbe53c54211",
+    "url": "/static/js/2.d6d163b2.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/static/js/2.d6d163b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca77e55802495c59b2a5",
+    "url": "/static/js/main.7fb91103.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
